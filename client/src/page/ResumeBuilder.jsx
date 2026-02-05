62da9ac5d8408181f1c7dc18e30390226ea44229
vispan77 <vishalpandey2332@gmail.com>
2026-02-05 20:36:49 +0530

    implemeted upload image functionality and component

--- client/src/page/ResumeBuilder.jsx
+++ client/src/page/ResumeBuilder.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom';
 import { dummyResumeData } from '../assets/assets';
 import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, FileText, FolderIcon, GraduationCap, Sparkles, User } from 'lucide-react';
+import PersonalInfoForm from '../components/PersonalinfoForm';
 
 function ResumeBuilder() {
 
@@ -100,6 +101,22 @@ function ResumeBuilder() {
                                         Next <ChevronRight className='size-4' />
                                     </button>
                                 </div>
+                            </div>
+
+                            {/* form content */}
+                            <div className='space-y-6'>
+                                {
+                                    activeSection.id === "personal" && (
+                                        <PersonalInfoForm 
+                                        data={resumeData.personal_info}
+                                        onChange={(data) => setResumeData(prev => ({...prev, personal_info: data}))}
+                                        removeBackground={removeBackground}
+                                        setRemoveBackground={setRemoveBackground}
+                                        
+                                        />
+                                    )
+                                }
+
                             </div>
 
                         </div>
