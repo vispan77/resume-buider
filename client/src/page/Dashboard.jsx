366ecd312e995f98c5f154016c33b0bf39cb35c9
vispan77 <vishalpandey2332@gmail.com>
2026-02-18 18:56:24 +0530

    mounted delete and updated dunctionality

--- client/src/page/Dashboard.jsx
+++ client/src/page/Dashboard.jsx
@@ -1,5 +1,6 @@
 import {
     FilePenLineIcon,
+    LoaderCircleIcon,
     PencilIcon,
     PlusIcon,
     TrashIcon,
@@ -64,18 +65,41 @@ function Dashboard() {
     }
 
     const editTitle = async (event) => {
-        event.preventDefault();
+        try {
+            event.preventDefault();
+            const { data } = await api.put(`/api/resumes/update`,
+                { resumeId: editResumeId, resumeData: { title } }, { headers: { Authorization: token } });
+            setAllResumes(allResumes.map((resume) => resume._id === editResumeId ? { ...resume, title } : resume));
+            setTitle("");
+            setEditResumeId("");
+            toast.success(data.message);
+        } catch (error) {
+            toast.error(error?.response?.data?.message || error.message);
+        }
+
     };
 
     const deleteResume = async (resumeId) => {
-        const confirm = window.confirm("Are you sure you want to delete this resume?");
-        if (confirm) {
-            setAllResumes((previous) => previous.filter((resume) => resume._id !== resumeId));
+        try {
+            const confirm = window.confirm("Are you sure you want to delete this resume?");
+            if (confirm) {
+                const { data } = await api.delete(`/api/resumes/delete/${resumeId}`, { headers: { Authorization: token } });
+                setAllResumes(allResumes.filter((resume) => resume._id !== resumeId));
+                toast.success(data.message);
+            }
+        } catch (error) {
+            toast.error(error?.response?.data?.message || error.message);
         }
+
     }
 
     const loadAllResumes = async () => {
-        setAllResumes(dummyResumeData);
+        try {
+            const { data } = await api.get("/api/users/resumes", { headers: { Authorization: token } });
+            setAllResumes(data.resumes);
+        } catch (error) {
+            toast.error(error?.response?.data?.message || error.message);
+        }
     };
 
     useEffect(() => {
@@ -211,7 +235,14 @@ function Dashboard() {
                                 </div>
 
                                 <button className="w-full py-2 bg-green-600 text-white rounded hover:bg-green-700 transition-colors">
-                                    Upload Resume
+
+                                    {
+                                        isLoading && (<LoaderCircleIcon className="animate-spin size-4 text-white" />)
+                                    }
+
+                                    {
+                                        isLoading ? "Uploading..." : "Upload Resume"
+                                    }
                                 </button>
 
                                 <XIcon onClick={() => { setShowUploadResume(false); setTitle("") }} className="lucide lucide-x absolute top-4 right-4 text-slate-400 hover:text-slate-600 cursor-pointer transition-colors" />
--- server/controllers.js/resumeControllers.js
+++ server/controllers.js/resumeControllers.js
@@ -131,7 +131,7 @@ export const updateResume = async (req, res) => {
         const { resumeId, resumeData, removeBackground } = req.body;
         const image = req.file;
 
-        const resumeDataCopy = JSON.parse(resumeData);
+        const resumeDataCopy = JSON.parse(JSON.stringify(resumeData));
 
         if (image) {
             const imageBufferData = fs.createReadStream(image.path);
@@ -164,3 +164,202 @@ export const updateResume = async (req, res) => {
         })
     }
 }
+
+
+
+
+
+// import imagekit from "../configs/imageKit.js";
+// import Resume from "../models/Resume.js";
+// import fs from "fs";
+
+// // controller for creating a new resume
+// // POST: /api/resumes/create
+// export const createResume = async (req, res) => {
+//     try {
+//         const userId = req.userId;
+//         const { title } = req.body;
+
+//         // create a new resume
+//         const newResume = await Resume.create({ userId, title });
+
+//         // return a success message
+//         return res.status(200).json({
+//             success: true,
+//             message: "Resume created Successfully",
+//             resume: newResume
+//         });
+
+//     } catch (error) {
+//         console.log("error :- ", error);
+//         return res.status(500).json({
+//             success: false,
+//             message: error.message
+//         });
+//     }
+// };
+
+
+// // controller for deleting a resume
+// // DELETE: /api/resumes/delete
+// export const deleteResume = async (req, res) => {
+//     try {
+//         const userId = req.userId;
+//         const { resumeId } = req.params;
+
+//         // delete a resume
+//         const deletedResume = await Resume.findOneAndDelete({
+//             userId,
+//             _id: resumeId
+//         });
+
+//         if (!deletedResume) {
+//             return res.status(404).json({
+//                 success: false,
+//                 message: "Resume not found"
+//             });
+//         }
+
+//         // return a success message
+//         return res.status(200).json({
+//             success: true,
+//             message: "Resume deleted Successfully",
+//         });
+
+//     } catch (error) {
+//         console.log("error :- ", error);
+//         return res.status(500).json({
+//             success: false,
+//             message: error.message
+//         });
+//     }
+// };
+
+
+// // get user resume by id
+// // GET: /api/resumes/get
+// export const getResumeById = async (req, res) => {
+//     try {
+//         const userId = req.userId;
+//         const { resumeId } = req.params;
+
+//         const resume = await Resume
+//             .findOne({ userId, _id: resumeId })
+//             .select("-__v -createdAt -updatedAt");
+
+//         if (!resume) {
+//             return res.status(404).json({
+//                 success: false,
+//                 message: "Resume not found"
+//             });
+//         }
+
+//         // return a success message
+//         return res.status(200).json({
+//             success: true,
+//             message: "Resume found Successfully",
+//             resume: resume
+//         });
+
+//     } catch (error) {
+//         console.log("error :- ", error);
+//         return res.status(500).json({
+//             success: false,
+//             message: error.message
+//         });
+//     }
+// };
+
+
+// // get resume by id public
+// // GET: /api/resumes/public
+// export const getPublicResumeById = async (req, res) => {
+//     try {
+//         const { resumeId } = req.params;
+
+//         const resume = await Resume.findOne({
+//             public: true,
+//             _id: resumeId
+//         });
+
+//         if (!resume) {
+//             return res.status(404).json({
+//                 success: false,
+//                 message: "Resume not found or resume is private"
+//             });
+//         }
+
+//         // return a success message
+//         return res.status(200).json({
+//             success: true,
+//             message: "Resume found Successfully",
+//             resume: resume
+//         });
+
+//     } catch (error) {
+//         console.log("error :- ", error);
+//         return res.status(500).json({
+//             success: false,
+//             message: error.message
+//         });
+//     }
+// };
+
+
+// // controller for updating resume
+// // PUT: /api/resumes/update
+// export const updateResume = async (req, res) => {
+//     try {
+//         const userId = req.userId;
+//         const { resumeId, resumeData, removeBackground } = req.body;
+//         const image = req.file;
+
+//         const resumeDataCopy = JSON.parse(JSON.stringify(resumeData));
+
+//         if (image) {
+//             const imageBufferData = fs.createReadStream(image.path);
+
+//             const response = await imagekit.files.upload({
+//                 file: imageBufferData,
+//                 fileName: `resume-${Date.now()}.png`,
+//                 folder: "user-resumes",
+//                 transformation: {
+//                     pre: "w-300,h-300,fo-face,z-0.75" +
+//                         (removeBackground ? ",e-bgremove" : "")
+//                 }
+//             });
+
+//             if (!resumeDataCopy.personal_info) {
+//                 resumeDataCopy.personal_info = {};
+//             }
+
+//             resumeDataCopy.personal_info.image = response.url;
+//         }
+
+//         const resume = await Resume.findOneAndUpdate(
+//             { userId, _id: resumeId },
+//             resumeDataCopy,
+//             { new: true }
+//         );
+
+//         if (!resume) {
+//             return res.status(404).json({
+//                 success: false,
+//                 message: "Resume not found"
+//             });
+//         }
+
+//         return res.status(200).json({
+//             success: true,
+//             message: "Resume updated Successfully",
+//             resume: resume
+//         });
+
+//     } catch (error) {
+//         console.log("error :- ", error);
+//         return res.status(500).json({
+//             success: false,
+//             message: error.message
+//         });
+//     }
+// };
